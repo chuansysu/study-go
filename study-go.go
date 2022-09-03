a7e5d07f4e6459adc4135189568ecc813eff3d6a
chuansysu <332833744@qq.com>
2022-09-04 03:27:42 +0800

    go addressable

--- study-go.go
+++ study-go.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	"study-go/yaml_"
+	"fmt"
+	"unsafe"
 )
+
+type s struct{ i int }
+
+const VERSION = "1.0"
 
 func main() {
 	//log_.DemoLog()
-	yaml_.DemoYaml()
+	//yaml_.DemoYaml()
 	//filepath_.DemoFilePath()
 	//io_.DemoIO()
+	// q := [3]int{1, 2, 3}[1:]
+	fmt.Printf("%p\n", &s{})
+	fmt.Println(unsafe.Pointer(&s{}))
+	q := &s{}
+	fmt.Printf("%p,%v\n", q, q)
+	fmt.Printf("%p\n", &[]int{1, 2, 3})
 }
